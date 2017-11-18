8983d617693fd1d52f6b5cf64dc1a4e7829b8658
Qiu <1025712173@qq.com>
2017-11-18 14:45:58 +0800

    初始化公共类库仓库

--- src/main/java/com/qiuyj/tools/AnnotationUtils.java
+++ src/main/java/com/qiuyj/tools/AnnotationUtils.java
@@ -1,5 +1,8 @@
 package com.qiuyj.tools;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+
 /**
  * 注解工具类
  * @author qiuyj
@@ -7,4 +10,7 @@ package com.qiuyj.tools;
  */
 public abstract class AnnotationUtils {
 
+  public static <A> A findAnnotation(AnnotatedElement ae, Class<? extends Annotation> anno) {
+    return null;
+  }
 }
