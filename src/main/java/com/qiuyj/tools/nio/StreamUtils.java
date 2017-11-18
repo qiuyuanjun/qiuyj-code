6c42b3718455d0a9d420ea871dfe4e751b26a85f
Qiu <1025712173@qq.com>
2017-11-19 01:52:45 +0800

    完善了StreamUtils工具类的，新增了根据文件路径得到字节数组和根据输入流得到字节数组的方法

--- src/main/java/com/qiuyj/tools/nio/StreamUtils.java
+++ src/main/java/com/qiuyj/tools/nio/StreamUtils.java
@@ -1,11 +1,96 @@
 package com.qiuyj.tools.nio;
 
+import com.qiuyj.tools.StringUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+
 /**
  * 二进制流操作工具类
  * @author qiuyj
  * @since 2017/11/18
  */
 public abstract class StreamUtils {
+  private static final int BUF_SIZE = 8192;
 
+  public static byte[] getBytes(String path) throws IOException {
+    path = StringUtils.cleanPath(path);
+    if (Objects.isNull(path))
+      throw new IllegalArgumentException("Parameter 'path' cannot be null");
+    FileChannel in = null;
+    ByteArrayOutputStream out = null;
+    byte[] b;
+    try {
+      in = FileChannel.open(Paths.get(path), StandardOpenOption.READ);
+      long fileLen = in.size();
+      MappedByteBuffer buf = in.map(FileChannel.MapMode.READ_ONLY, 0L, fileLen);
+      int cycle = (int) fileLen / BUF_SIZE,
+          last = (int) fileLen % BUF_SIZE;
+      if (cycle > 0) {
+        out = new ByteArrayOutputStream(BUF_SIZE);
+        b = new byte[BUF_SIZE];
+        for (int i = 0; i < cycle; i++) {
+//          buf.flip();  这里无需flip，因为这个buf一直是读模式，没有从读模式变成写模式
+          buf.get(b);
+          out.write(b);
+//          buf.clear(); // 这里一定不能用clear，这样会将position重新置为0，导致一直重复读
+        }
+      } else {
+        out = new ByteArrayOutputStream(last);
+        b = new byte[last];
+      }
+      if (last > 0) {
+        buf.get(b, 0, last);
+        out.write(b, 0, last);
+      }
+      b = out.toByteArray();
+    } finally {
+      closeQuietly(in);
+      closeQuietly(out);
+    }
+    return b;
+  }
+
+  public static byte[] getBytes(InputStream in, boolean close) throws IOException {
+    ReadableByteChannel inChannel = Channels.newChannel(in);
+    ByteArrayOutputStream out = null;
+    byte[] b;
+    try {
+      ByteBuffer buf = ByteBuffer.allocate(BUF_SIZE);
+      out = new ByteArrayOutputStream(BUF_SIZE);
+      int len;
+      while ((len = inChannel.read(buf)) > -1) {
+//        buf.flip();  这里无需flip，因为一直是读模式
+        out.write(buf.array(), 0, len);
+        buf.clear(); // 这里一定要clear，具体原因参考javaAPI
+      }
+      b = out.toByteArray();
+    } finally {
+      closeQuietly(out);
+      if (close)
+        closeQuietly(inChannel);
+    }
+    return b;
+  }
+
+  public static void closeQuietly(Closeable stream) {
+    if (Objects.nonNull(stream)) {
+      try {
+        stream.close();
+      } catch (IOException e) {
+        // ignore
+      }
+    }
+  }
 
 }
