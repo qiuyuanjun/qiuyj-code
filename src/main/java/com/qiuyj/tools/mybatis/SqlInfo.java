cf5ec334d055fede6b9b5a8d153696029d182442
Qiu <1025712173@qq.com>
2017-11-19 23:03:31 +0800

    新增支持Ignore注解，同时优化了SqlInfo，剔除静态属性

--- src/main/java/com/qiuyj/tools/mybatis/SqlInfo.java
+++ src/main/java/com/qiuyj/tools/mybatis/SqlInfo.java
@@ -5,12 +5,14 @@ import com.qiuyj.tools.ClassUtils;
 import com.qiuyj.tools.ReflectionUtils;
 import com.qiuyj.tools.StringUtils;
 import com.qiuyj.tools.mybatis.annotation.Column;
+import com.qiuyj.tools.mybatis.annotation.Ignore;
 import com.qiuyj.tools.mybatis.annotation.PrimaryKey;
 import com.qiuyj.tools.mybatis.annotation.Table;
 import com.qiuyj.tools.mybatis.mapper.Mapper;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -37,39 +39,63 @@ public final class SqlInfo {
     // 接着得到主键名和其他列名
     Field[] allDeclaredFields = ClassUtils.getAllDeclaredFields(beanType);
     for (Field field : allDeclaredFields) {
+      // 跳过静态属性
+      if (Modifier.isStatic(field.getModifiers()))
+        continue;
+      Method getter = null;
+      // 检查@Ignore
+      if (AnnotationUtils.hasAnnotation(field, Ignore.class))
+        continue;
+      else {
+        // 查找对应的getter方法
+        try {
+          getter = ReflectionUtils.getDeclaredMethod(beanType, fieldToGetterName(field), field.getType());
+          if (AnnotationUtils.hasAnnotation(getter, Ignore.class))
+            continue;
+        } catch (IllegalStateException e) {
+          // ignore
+        }
+      }
       boolean primaryKeyFlag = false;
       if (Objects.isNull(primaryKey)) {
         if (AnnotationUtils.hasAnnotation(field, PrimaryKey.class)) {
           primaryKey = new PropertyColumnMapping();
           primaryKeyFlag = true;
         } else {
-          try {
-            // 得到对应的get方法
-            Method getter = ReflectionUtils.getDeclaredMethod(beanType, fieldToGetterName(field), field.getType());
+          if (Objects.isNull(getter)) {
+            try {
+              getter = ReflectionUtils.getDeclaredMethod(beanType, fieldToGetterName(field), field.getType());
+            } catch (IllegalStateException e) {
+              // ignore;
+            }
+          }
+          if (Objects.nonNull(getter)) {
             // 判断在getter方法上是否有PrimaryKey注解
             if (AnnotationUtils.hasAnnotation(getter, PrimaryKey.class)) {
               primaryKey = new PropertyColumnMapping();
               primaryKeyFlag = true;
             }
-          } catch (Throwable throwable) {
-            // ignore
           }
         }
       }
-      // 接下来处理Column
+      // 接下来处理@Column
       Column currCol = AnnotationUtils.findAnnotation(field, Column.class);
       String columnName = null;
       if (Objects.nonNull(currCol))
         columnName = currCol.value();
-      if (Objects.isNull(columnName)) {
+      else {
         // 此时去看对应的getter方法上面是否有Column注解
-        try {
-          Method getter = ReflectionUtils.getDeclaredMethod(beanType, fieldToGetterName(field), field.getType());
+        if (Objects.isNull(getter)) {
+          try {
+            getter = ReflectionUtils.getDeclaredMethod(beanType, fieldToGetterName(field), field.getType());
+          } catch (IllegalStateException e) {
+            // ignore
+          }
+        }
+        if (Objects.nonNull(getter)) {
           Column methodCol = AnnotationUtils.findAnnotation(getter, Column.class);
           if (Objects.nonNull(methodCol))
             columnName = methodCol.value();
-        } catch (IllegalStateException e) {
-          // ignore
         }
       }
       if (Objects.isNull(columnName))
