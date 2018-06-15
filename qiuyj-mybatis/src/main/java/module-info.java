4909e2e00b1fcb98385d4466fff5a8ef75767533
qiuyuanjun <18707097515@163.com>
2018-06-15 12:15:17 +0800

    适配java10

--- qiuyj-mybatis/src/main/java/module-info.java
+++ qiuyj-mybatis/src/main/java/module-info.java
@@ -2,4 +2,14 @@ module qiuyj.mybatis {
   requires qiuyj.commons;
   requires mybatis;
   requires java.sql;
+
+  exports com.qiuyj.mybatis;
+  exports com.qiuyj.mybatis.checker;
+  exports com.qiuyj.mybatis.annotation;
+  exports com.qiuyj.mybatis.config;
+  exports com.qiuyj.mybatis.engine;
+  exports com.qiuyj.mybatis.key;
+  exports com.qiuyj.mybatis.mapper;
+  exports com.qiuyj.mybatis.sqlbuild;
+  exports com.qiuyj.mybatis.sqlbuild.typehandler;
 }
